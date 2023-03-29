caf76d4eae538dabc4ebe573cf2952a42f48a5c6
Yoon Yongjin <abcd950217@gamil.com>
2023-03-29 19:11:55 +0900

    update: [윤용진] 판매자가 공지사항 정보 추가 및 공지사항 목록이 보이는 서비스[WMC-PROCESS 20]

--- Backend/src/main/java/com/example/Backend/service/notice/response/NoticeListResponse.java
+++ Backend/src/main/java/com/example/Backend/service/notice/response/NoticeListResponse.java
@@ -9,7 +9,7 @@ import lombok.Setter;
 @AllArgsConstructor
 public class NoticeListResponse {
 
-    private long noticeId;
+    private Long noticeId;
     private String title;
     private String writer;
     private String content;
