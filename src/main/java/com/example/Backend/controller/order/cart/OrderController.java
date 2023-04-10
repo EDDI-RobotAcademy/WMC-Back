1fcf5578a789439dc2f5c0dd513601a069bcf010
jonginhan <instantgogo@gmail.com>
2023-04-11 02:13:35 +0900

    feat: [한종인] 상품 구매 시, 상품 재고 변경[WMC-PROCESS 52]

--- src/main/java/com/example/Backend/controller/order/cart/OrderController.java
+++ src/main/java/com/example/Backend/controller/order/cart/OrderController.java
@@ -1,16 +1,16 @@
 package com.example.Backend.controller.order.cart;
 
+import com.example.Backend.service.order.request.OrderItemRequest;
+import com.example.Backend.service.product.ProductService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -19,8 +19,10 @@ import java.net.URL;
 @CrossOrigin(origins = "http://localhost:8080", allowedHeaders = "*")
 public class OrderController {
 
+    final private ProductService productService;
+
     @PostMapping("/kakaoPay")
-    public String kakaoPay() throws IOException {
+    public String kakaoPay(@RequestBody List<OrderItemRequest> orderItems) throws IOException {
         URL url = new URL("https://kapi.kakao.com/v1/payment/ready");
         try {
             HttpURLConnection conn = (HttpURLConnection) url.openConnection();
@@ -38,6 +40,9 @@ public class OrderController {
             InputStream is;
             if(responseCode == 200) {
                 is = conn.getInputStream();
+                for (OrderItemRequest orderItem : orderItems) {
+                    productService.decreaseProductStock(orderItem.getProductId(), orderItem.getQuantity());
+                }
             } else {
                 is = conn.getErrorStream();
             }
