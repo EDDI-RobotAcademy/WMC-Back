7371cc1060020b051ca227fe0c0fcfc8f1ce7214
Yoon Yongjin <abcd950217@gmail.com>
2023-04-10 18:30:10 +0900

    fix: [윤용진] 공지사항 목록 클릭 시 상세 페이지에 내용이 보여지는 시스템[WMC-PROGRESS 38]

--- src/test/java/com/example/Backend/NoticeTest.java
+++ src/test/java/com/example/Backend/NoticeTest.java
@@ -1,12 +1,12 @@
 package com.example.Backend;
 
-
+import com.example.Backend.entity.notice.Notice;
 import com.example.Backend.repository.notice.NoticeImageDataRepository;
 import com.example.Backend.repository.notice.NoticeRepository;
 import com.example.Backend.service.notice.NoticeService;
 import com.example.Backend.service.notice.request.NoticeRequest;
 import com.example.Backend.service.notice.response.NoticeListResponse;
-
+import com.example.Backend.service.notice.response.NoticeReadResponse;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -50,5 +50,28 @@ public class NoticeTest {
         assertTrue(notices.size() > 0);
     }
 
+    @Test
+    public void 공지사항_읽기_테스트() {
+        noticeRepository.deleteAll();
+        assertEquals(0, noticeRepository.count());
 
+        List<String> savedFilePaths = Arrays.asList(
+                "src/assets/noticeImages/image1.jpg",
+                "src/assets/noticeImages/image1.jpg",
+                "src/assets/noticeImages/image1.jpg"
+        );
+
+        Boolean isRegistered = noticeService.register(new NoticeRequest(
+                "test", "test입니다", "test내용", savedFilePaths
+        ));
+
+        assertTrue(isRegistered);
+
+        List<Notice> notices = noticeRepository.findAll();
+        assertEquals(1, notices.size());
+
+        Notice notice = notices.get(0);
+        NoticeReadResponse noticeReadResponse = noticeService.read(notice.getNoticeId());
+        assertEquals(notice.getNoticeId(), noticeReadResponse.getNoticeId());
+    }
 }
