38674f0771e6a044e614866de8fc347d88a4be28
jonginhan <instantgogo@gmail.com>
2023-05-03 06:34:23 +0900

    test: [한종인] test code 비활성화

--- src/test/java/com/example/Backend/CategoryTest.java
+++ src/test/java/com/example/Backend/CategoryTest.java
@@ -1,29 +1,29 @@
-package com.example.Backend;
-
-import com.example.Backend.service.category.CategoryService;
-import com.example.Backend.service.category.QuestionCategoryService;
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-public class CategoryTest {
-
-    @Autowired
-    private CategoryService categoryService;
-
-    @Autowired
-    private QuestionCategoryService questionCategoryService;
-
-    @Test
-    public void category_리스트_불러오기() {
-
-        System.out.println("카테고리 리스트 입니다: " + categoryService.getAllCategories());
-    }
-
-    @Test
-    public void Question_Category_리스트_불러오기() {
-
-        System.out.println("카테고리 리스트 입니다: " + categoryService.getAllCategories());
-    }
-}
+//package com.example.Backend;
+//
+//import com.example.Backend.service.category.CategoryService;
+//import com.example.Backend.service.category.QuestionCategoryService;
+//import org.junit.jupiter.api.Test;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//public class CategoryTest {
+//
+//    @Autowired
+//    private CategoryService categoryService;
+//
+//    @Autowired
+//    private QuestionCategoryService questionCategoryService;
+//
+//    @Test
+//    public void category_리스트_불러오기() {
+//
+//        System.out.println("카테고리 리스트 입니다: " + categoryService.getAllCategories());
+//    }
+//
+//    @Test
+//    public void Question_Category_리스트_불러오기() {
+//
+//        System.out.println("카테고리 리스트 입니다: " + categoryService.getAllCategories());
+//    }
+//}
